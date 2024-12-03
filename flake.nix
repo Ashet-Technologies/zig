ebacf6d80bc8f2c1946d1424e71a14955d4ccac0
Felix "xq" Queißner <git@random-projects.net>
2024-12-03 21:35:55 +0100

    Adds flake.nix for CI and local development.

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Zig compiler development.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # Used for shell.nix
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;
+            [
+              cmake
+              gdb
+              libxml2
+              ninja
+              qemu
+              wasmtime
+              zlib
+              xz
+            ]
+            ++ (with llvmPackages_18; [
+              clang
+              clang-unwrapped
+              lld
+              llvm
+            ]);
+
+          hardeningDisable = ["all"];
+        };
+
+        # For compatibility with older versions of the `nix` binary
+        devShell = self.devShells.${system}.default;
+      }
+    );
+}
